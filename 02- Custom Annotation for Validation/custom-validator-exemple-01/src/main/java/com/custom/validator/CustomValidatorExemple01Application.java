59e650e30f10a791be513295af416ecd458c2118
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-10-01 15:41:49 +0200

    Task 6: create spring boot custom annotation for validation project

---
+++ 02- Custom Annotation for Validation/custom-validator-exemple-01/src/main/java/com/custom/validator/CustomValidatorExemple01Application.java
@@ -0,0 +1,13 @@
+package com.custom.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomValidatorExemple01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomValidatorExemple01Application.class, args);
+    }
+
+}
