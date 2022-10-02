4d2694f4fdb54dd7ea0239088f57596dbb81e77f
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-10-02 10:25:47 +0100

    Task 10: create spring boot data transfer object (manual dto creation) project

---
+++ 03- Data Transfer Object and Mapper/Data-Transfer-Object-Manual/src/main/java/com/datatransferobjectmanual/DataTransferObjectManualApplication.java
@@ -0,0 +1,13 @@
+package com.datatransferobjectmanual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransferObjectManualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTransferObjectManualApplication.class, args);
+    }
+
+}
---
+++ 03- Data Transfer Object and Mapper/Data-Transfer-Object-Manual/src/test/java/com/datatransferobjectmanual/DataTransferObjectManualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datatransferobjectmanual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransferObjectManualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
