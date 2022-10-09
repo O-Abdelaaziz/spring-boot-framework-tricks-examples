53b8beb532bf203ace6b34b74895c6c967321f0e
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-10-09 13:24:30 +0100

    Task 13: create spring boot integration test exemple 1

---
+++ 04- Testing/integration-test-exemple-1/src/main/java/com/integration/test/IntegrationTestExemple1Application.java
@@ -0,0 +1,13 @@
+package com.integration.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTestExemple1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationTestExemple1Application.class, args);
+    }
+
+}
