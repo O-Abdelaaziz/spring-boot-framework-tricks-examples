f1053c84f88e3ccf70ff9207fc04320a5d554094
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-10-23 09:06:40 +0100

    Task 15: create spring boot complete layers testing exemple 1

---
+++ 04- Testing/complete-layers-test-example-1/src/main/java/com/complete/layers/test/CompleteLayersTestExample1Application.java
@@ -0,0 +1,13 @@
+package com.complete.layers.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteLayersTestExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteLayersTestExample1Application.class, args);
+    }
+
+}
---
+++ 04- Testing/complete-layers-test-example-1/src/test/java/com/complete/layers/test/CompleteLayersTestExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complete.layers.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteLayersTestExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
